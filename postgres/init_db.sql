2a199f37bafceca67048083d8d6b2ac2ed9b9ac9
Kevin Rafael Marin Cobena <kevin.marinc@outlook.es>
2025-03-09 17:49:55 -0500

    rename

--- postgres/init_db.sql
+++ postgres/init_db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "public"."users" (
   "id" bigserial,
-  "key" varchar(255) UNIQUE NOT NULL,
   "document" varchar(255) UNIQUE NOT NULL,
+  "hyperledger_key" varchar(255) UNIQUE NOT NULL,
   "secret_key" varchar(255) UNIQUE NOT NULL,
   "iv" varchar(255) UNIQUE NOT NULL,
   "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
