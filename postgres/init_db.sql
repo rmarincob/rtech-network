7126b6eb5221b485857254e7746317368930010a
Kevin Rafael Marin Cobena <kevin.marinc@outlook.es>
2025-03-10 16:26:27 -0500

    fixed postgres container

--- postgres/init_db.sql
+++ postgres/init_db.sql
@@ -1,9 +1,23 @@
-CREATE TABLE "public"."users" (
-  "id" bigserial,
-  "document" varchar(255) UNIQUE NOT NULL,
-  "hyperledger_key" varchar(255) UNIQUE NOT NULL,
-  "secret_key" varchar(255) UNIQUE NOT NULL,
-  "iv" varchar(255) UNIQUE NOT NULL,
+CREATE DATABASE security
+WITH
+  OWNER = 'postgres'
+  ENCODING = 'UTF8'
+;
+
+\c security;
+
+CREATE SCHEMA IF NOT EXISTS keycloak AUTHORIZATION postgres;
+GRANT ALL ON SCHEMA keycloak TO postgres;
+
+CREATE SCHEMA IF NOT EXISTS hyperledger AUTHORIZATION postgres;
+GRANT ALL ON SCHEMA hyperledger TO postgres;
+
+CREATE TABLE "hyperledger"."users" (
+  "id" BIGSERIAL,
+  "document" VARCHAR(255) UNIQUE NOT NULL,
+  "hyperledger_key" VARCHAR(255) UNIQUE NOT NULL,
+  "secret_key" VARCHAR(255) UNIQUE NOT NULL,
+  "iv" VARCHAR(255) UNIQUE NOT NULL,
   "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY ("id")
 );
